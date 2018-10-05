1464314fb32a000ab2ed90047814dce8f88640a9
swati-kale <33409491+swati-kale@users.noreply.github.com>
2018-10-05 13:57:48 -0400

    Update index.php

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ try {
     
     
     
- $redis = new Predis\Client(array("scheme" => "tcp","host" => "172.30.162.7","port" => 19861,'password' => "testdb");
+ $redis = new Predis\Client(array("scheme" => "tcp","host" => "172.30.162.7","port" => 19861,"password" => "testdb");
     
    //$sentinels = ['tcp://172.30.85.91'];
 //$options   = ['replication' => 'sentinel', 'service' => 'mymaster'];
