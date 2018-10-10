d51528369d9bd3d741996c63644d03cf9e6ba190
swati-kale <33409491+swati-kale@users.noreply.github.com>
2018-10-09 20:07:04 -0400

    Update index.php

--- index.php
+++ index.php
@@ -20,13 +20,14 @@ try {
     
    // $redis = new Predis\Client(array("scheme" => "tcp","host" => "10.128.5.10","port" => 8001));
     
-$redis = new Predis\Client(array("scheme" => "tcp","host" => "172.30.57.94","port" => 6379));
+//$redis = new Predis\Client(array("scheme" => "tcp","host" => "172.30.57.94","port" => 6379));
+    $redis = new Predis\Client(array("scheme" => "tcp","host" => "10.129.2.81","port" => 14626,"password"=> "testdb"));
    //$sentinels = ['tcp://172.30.85.91'];
 //$options   = ['replication' => 'sentinel', 'service' => 'mymaster'];
 //$redis = new Predis\Client($sentinels, $options);
     
     echo "Successfully connected to Redis";
-$redis->set("hello_world", "Hi from Swati Kale!");                                     
+$redis->set("hello_world", "**********Hi from Swati Kale!*************");                                     
 $value = $redis->get("hello_world");                                            
 var_dump($value);    
     
